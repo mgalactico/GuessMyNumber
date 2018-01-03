8ddb33b0079a3170dd099969fabe7b6744ab5a7f
mgalactico <mjarrin@outlook.com>
2018-01-03 14:22:16 -0500

    Collected common code in function.

--- GuessMyNumber.py
+++ GuessMyNumber.py
@@ -5,47 +5,43 @@
 # too high or too low? Using bisection search, the computer will guess the user's secret number!
 
 
-def verify_response(check_response):
-    while check_response not in acceptedResponses:
+def verify_response():
+    user_response = input(
+        'Enter \'h\' to indicate the guess is too high. Enter \'l\' to indicate the guess is '
+        'too low. Enter \'c\' to indicate I guessed correctly.')
+    while user_response not in acceptedResponses:
         print('Sorry, I did not understand your input.')
-        print('Is your secret number', answer, '?')
-        check_response = input(
-            'Enter \'h\' to indicate the guess is too high. Enter \'l\' to indicate the guess is too low. Enter \'c\' to indicate I guessed correctly.')
-    return check_response
+        print('Is your secret number', guess, '?')
+        user_response = input(
+            'Enter \'h\' to indicate the guess is too high. Enter \'l\' to indicate the guess '
+            'is too low. Enter \'c\' to indicate I guessed correctly.')
+    return user_response
 
 
 acceptedResponses = ['h', 'l', 'c']
 start = 0
-answer = middle = 50
+middle = 50
 end = 100
 print('Please think of a number between 0 and 100!')
 print('Is your secret number', middle, '?')
 
-check_response = input(
-    'Enter \'h\' to indicate the guess is too high. Enter \'l\' to indicate the guess is too low. Enter \'c\' to indicate I guessed correctly.')
-response = verify_response(check_response)
+response = verify_response()
 
 while response != 'c':
     if response == 'l':
         guess = int((middle + end) / 2)
         start = middle
         middle = int((start + end) / 2)
-        answer = guess
-        print('Is your secret number', answer, '?')
+        print('Is your secret number', guess, '?')
 
-        check_response = input(
-            'Enter \'h\' to indicate the guess is too high. Enter \'l\' to indicate the guess is too low. Enter \'c\' to indicate I guessed correctly.')
-        response = verify_response(check_response)
+        response = verify_response()
 
     elif response == 'h':
         guess = int((start + middle) / 2)
         end = middle
         middle = int((start + end) / 2)
-        answer = guess
-        print('Is your secret number', answer, '?')
+        print('Is your secret number', guess, '?')
 
-        check_response = input(
-            'Enter \'h\' to indicate the guess is too high. Enter \'l\' to indicate the guess is too low. Enter \'c\' to indicate I guessed correctly.')
-        response = verify_response(check_response)
+        response = verify_response()
 
-print('Game over. Your secret number was:', answer)
+print('Game over. Your secret number was:', guess)
